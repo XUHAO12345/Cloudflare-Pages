42def01334bac272870e55c8bcb6464ccdf28b1f
haoge <45056848+XUHAO12345@users.noreply.github.com>
2023-07-31 10:49:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'heooll-18872918455.b4a.run'
+      url.hostname = 'hagege-18872918455.b4a.run'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
